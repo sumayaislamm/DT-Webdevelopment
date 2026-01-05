0bfc48bfb58d59dec1de5fb6ca550a596ad179a7
Sumaya <sheikhsumaya622@gmail.com>
2026-01-06 03:00:02 +0600

    almost complete

--- JS/script.js
+++ JS/script.js
@@ -46,8 +46,7 @@ dummyData.tasks[0].assets.forEach(asset => {
 
     // -----Description
     const desc = document.createElement("p");
-    desc.innerHTML = `<p><span class="dec_title">Description: </span>${asset.asset_description}</p>`;
-    desc.classList.add("padding")
+    desc.innerHTML = `<p class="padding"><span class="dec_title">Description: </span>${asset.asset_description}</p>`;
     card.appendChild(desc);
 
 
@@ -65,26 +64,94 @@ dummyData.tasks[0].assets.forEach(asset => {
     else if (asset.asset_content_type === "threadbuilder") {
         card.innerHTML += `
       <div class="thread_head"><span> <img src="../images/icons/upper sign.png" alt=""/> </span> 
-      <h4>Thread A</h4> 
-      <textarea placeholder="Enter text for Thread A"></textarea>
-      <textarea placeholder="Enter Sub-thread details"></textarea>
-      <select><option>Select Category</option></select>
-      <select><option>Select Process</option></select>
-      <button class="btn">+ Add Sub-thread</button>
-      <textarea placeholder="Summary for Thread A"></textarea>
+      <p>Thread A</p> </div>
+      <div class="thread_block">
+      <div class="single_texarea">
+      <label for="subthread1">Sub thread 1</label>
+      <textarea id="subthread1" placeholder="Enter Text here"></textarea>
+      </div>
+      <div class="single_texarea"> 
+      <label for="interpretation1">Sub Interpretation 1</label>
+      <textarea id="interpretation1" placeholder="Enter Text here"></textarea>
+      </div>
+      </div>
+      <div class="three_button"> 
+      <img src="../images/icons/Group.png" alt=""/>
+      
+      <select>
+      <option>Select Categ</option>
+      <option>A</option>
+      <option>B</option>
+      </select>
+      
+     
+      <select>
+      <option>Select Proces</option>
+      <option>A</option>
+      <option>B</option>
+      </select>
+      </div>
+      <button class="btn btn-thread">Add Sub-thread</button>
+      <div class="last_texarea"> 
+      <label for="interpretation1">Sub Interpretation 1</label>
+      <textarea id="interpretation1" placeholder="Enter Text Here"></textarea>
+      </div>
     `;
     }
     else if (asset.asset_content_type === "article") {
         if (asset.asset_title === "Structure your pointers") {
-            card.innerHTML += `<textarea placeholder="Write your article here..."></textarea>`;
-        } else if (asset.asset_title === "4SA Method") {
             card.innerHTML += `
-        <textarea placeholder="Introduction: How to bring an idea into progress?"></textarea>
-        <textarea placeholder="Thread A: Strategy development details..."></textarea>
-        <textarea placeholder="Example 1: How will you put a concept into progress?"></textarea>
-        <button class="btn">See More</button>
-      `;
+        <div class="article_block">
+            <label>Title</label>
+            <textarea class="article_title" "></textarea>
+
+            <label>Content</label>
+            <div class="toolbar">
+                <button type="button">File</button>
+                <button type="button">Edit</button>
+                <button type="button">View</button>
+                <button type="button">Insert</button>
+                <button type="button">Format</button>
+                <button type="button">Tools</button>
+                <button type="button">Table</button>
+                <button type="button">Help</button>
+                
+                <button type="button"><img src="../images/icons/arrow-curve-left-right.png" alt=""/></button>
+                <button type="button"><img src="../images/icons/arrow-curve-left-down.png" alt=""/></button>
+                <button type="button"><img src="../images/icons/arrow-expand-02.png" alt=""/></button>
+                <button type="button">Paragraph</button>
+            </div>
+            <textarea class="article_content" ></textarea>
+        </div>
+    `;
         }
+
+        else if (asset.asset_title === "4SA Method") {
+    card.innerHTML += `
+        <div class="method_container">
+
+            <div class="method_block">
+                <h6 class="introduction">Introduction</h6>
+                <p>The 4SA Method, How to bring a idea into progress ?</p>
+                <button class="see_more_btn">See More</button>
+            </div>
+
+            <div class="method_block">
+                <h6 class="without_introduction">Thread A</h6>
+                <p >How are you going to develop your strategy? Which method are you going to use to develop a strategy? What if the project is lengthy?</p>
+                <button class="see_more_btn">See More</button>
+                <div class="method_block">
+                <h6 class="without_introduction">Example 1</h6>
+                <p readonly>You have a concept, How will you put it into progress?</p>
+            </div>
+            </div>
+
+          
+
+        </div>
+    `;
+}
+
     }
 
     cardsContainer.appendChild(card);
